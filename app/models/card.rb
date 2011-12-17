3a2abd47feea1fb818c9f52afbeda77d60953292
Ozzie Gooen <Ozzie_Gooen@hmc.edu>
2011-12-16 20:56:40 -0800

    first commit

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,8 @@
 class Card < ActiveRecord::Base
+
+  scope :root_cards, where(:parent_id => nil)
+	belongs_to :parent, :class_name => "Card", :foreign_key => "parent_id"
+	has_many :children, :class_name => "Card", :foreign_key => "parent_id"
+
+
 end
