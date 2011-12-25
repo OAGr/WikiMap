f0168aea76b395dcba6fef24e820f8d36bd59977
Ozzie Gooen <Ozzie_Gooen@hmc.edu>
2011-12-25 00:13:10 -0500

    finding a problem

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Ideaspan::Application.routes.draw do
 
 match '/auth/:provider/callback', :to => 'sessions#create'
 
+  root :to => 'cards#index'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
