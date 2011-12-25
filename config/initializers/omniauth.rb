5daa0878ceb4e2cb2644bc699c701166b454701c
Ozzie Gooen <Ozzie_Gooen@hmc.edu>
2011-12-25 05:14:03 -0500

    added facebook key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,11 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-
-
-      provider :facebook, "203758613046153", "2d14bb7b20eb82ce5c88367425548509"
-
+if Rails.env == 'development' || Rails.env == 'test'
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, "203758613046153", "2d14bb7b20eb82ce5c88367425548509"
+  end
+else
+  # Production
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, "309363909097151", "dbac439fc9ab2cbbcb71069bcf99bde4"
+  end
 end
+
